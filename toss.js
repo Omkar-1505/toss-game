e3ec2812aea909885480efcb9598007f9ee77e83
Omkar-1505 <omkarpadheetheboss@gmail.com>
2025-05-22 11:26:39 +0530

    bugs fixing

--- toss.js
+++ toss.js
@@ -1,64 +1,43 @@
 var x = Math.floor(Math.random() * 2) + 1; // Random number 1 or 2
 var v; // track the current player
-var r; // track the toss result
+var r; // track the choice
 var y; // for coin type
+var img;//image selection
 
-function player1() {
-    v = 1; 
+function playeractive(select){
+    if(select.value === "1") v=1;
+    else v=2;
 }
-
-function player2() {
-    v = 0; 
+function cointype(select){
+    if(select.value === "1") y=ICC;
+    else y=ipl;
 }
-
-function ipl_coin() {
-    y = "ipl"; 
-    //console.log("Using IPL coin");
+function choice(select){
+    if(select.value === "1") r=1;
+    else r=2;
 }
-
-function icc_coin() {
-    y = "ICC"; 
-    //console.log("Using ICC coin");
+function toss(){
+    img=document.querySelector("image");
+    img.src=images/y_x.jpg;
 }
-function Heads() {
-    r = 1; 
-}
-
-function Tails() {
-    r = 2; 
-}
-function toss() {
-    var image = document.querySelector("img");
-    if (y==="ICC"){
-        if(x === 1) {
-            image.src = "images/ICC_1.jpg"; 
-        } else {
-            image.src = "images/ICC_2.jpg";
+var res;//results
+function result(){
+    toss();
+    res=document.querySelector("work");
+    if(v===1){
+        if(r===x){
+            res.innerHTML="PLAYER 1 WINS THE TOSS";
         }
+        else res.innerHTML="PLAYER 2 WINS THE TOSS";
     }
     else{
-        if(x === 1) {
-            image.src = "images/ipl_1.jpg";
-        } else {
-            image.src = "images/ipl_2.jpg";
+        if(r===x){
+            res.innerHTML="PLAYER 2 WINS THE TOSS";
         }
+        else res.innerHTML="PLAYER 1 WINS THE TOSS";
     }
 }
-//document.querySelector("button").addEventListener("click",toss())
-
-function displayResult() {
-    var w = document.querySelector(".result");
-    if (r === x) {
-        if (v === 1) {
-            w.innerHTML = "PLAYER 1 WINS THE TOSS!!!";
-        } else {
-            w.innerHTML = "PLAYER 2 WINS THE TOSS!!!";
-        }
-    } else {
-        if (v === 1) {
-            w.innerHTML = "PLAYER 2 WINS THE TOSS!!!";
-        } else {
-            w.innerHTML = "PLAYER 1 WINS THE TOSS!!!";
-        }
-    }
-}
+/*document.querySelector("button").addEventListener("click",){
+    toss();
+    result();
+}*/
